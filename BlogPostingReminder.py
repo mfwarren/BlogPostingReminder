6e2b05304609d20220cb65d47c1f73b0a8f49d76
Matt Warren <matt.warren@gmail.com>
2019-01-14 14:57:38 -0500

    checking in the script

---
+++ BlogPostingReminder.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# written by Matt Warren
+# https://mattwarren.co/
+
+import datetime
+import os
+
+import feedparser
+from croniter import croniter
+from twx.botapi import TelegramBot
+
+SETTINGS = [
+    {'name': 'Fosterous', 'url': 'https://medium.com/feed/fosterous', 'schedule': '12 * * * mon'},
+    {
+        'name': 'mattwarren.co',
+        'url': 'http://feeds.feedburner.com/AllThingsOtis',
+        'schedule': '12 * * * mon',
+    },
+]
+TELEGRAM_BOT_APIKEY = os.environ['TELEGRAM_BOT_APIKEY']
+TELEGRAM_USER_ID = int(os.environ['TELEGRAM_USER_ID'])
+
+
+bot = TelegramBot(TELEGRAM_BOT_APIKEY)
+bot.update_bot_info().wait()
+
+today = datetime.datetime.today()
+
+
+def check_blog(blog):
+    print(f"checking {blog['name']}")
+    d = feedparser.parse(blog['url'])
+    lastPost = d.entries[0]
+
+    if 'modified_parsed' in lastPost.keys():
+        date_parsed = lastPost.modified_parsed
+    elif 'published_parsed' in lastPost.keys():
+        date_parsed = lastPost.published_parsed
+
+    pubDate = datetime.datetime(
+        date_parsed[0],
+        date_parsed[1],
+        date_parsed[2],
+        date_parsed[3],
+        date_parsed[4],
+        date_parsed[5],
+    )
+
+    iter = croniter(blog['schedule'], pubDate)
+    next_post_expected_at = iter.get_next(datetime.datetime)
+
+    if next_post_expected_at < today:
+        msg = f"{blog['name']} needs attention! last post {str((today-pubDate).days)} days ago."
+        bot.send_message(TELEGRAM_USER_ID, msg).wait()
+    else:
+        msg = f"{blog['name']} is fresh! last post {str((today-pubDate).days)} days ago."
+        print(msg)
+
+
+if __name__ == '__main__':
+    for blog in SETTINGS:
+        check_blog(blog)
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# BlogPostingReminder
+# Blog Posting Reminder
 Simple script I use to remind myself to write weekly blog posts.  sends notification to me via telegram
+
+To use, edit the SETTINGS variable with settings for your blog.
+set environment variables for TELEGRAM API KEY and USER_ID (user id is an integer value and tricky to find)
+
+Schedule the script to run, as often as you want to be nagged.
+
+Happy blogging!
